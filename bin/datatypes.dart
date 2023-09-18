e60d9fb58535999a121d412f4879555055ce9cbe
Vinay20011 <vinaybharathan@gmail.com>
2023-09-18 12:39:29 +0530

    Initial commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name ='vishnu';
+  int age =25;
+  int phone =96737373;
+  double cgpa =8.3;
+  String email ='puli@gmail.com';
+  print('my name is $name');
+  print('and age is$age');
+  print('my cgpa is $cgpa');
+}
---
+++ bin/first_pgrm.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  print("vinay");
+  print(22);
+  print('my phone number is 1638383');
+  print(6.7);
+}
